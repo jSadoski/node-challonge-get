63de20db8a32345207cb26ba269eacd404c542a3
Joe Sadoski <jdsadoski@gmail.com>
2020-08-16 01:17:57 -0400

    Tests tournaments parameters

--- test/app.spec.js
+++ test/app.spec.js
@@ -2,27 +2,67 @@ const assert = require('chai').assert;
 const challonge = require('../challongeAPI.js');
 const credentials = require('./.credentials.json');
 
-describe('Tests retrieve', function () {
+describe('API', function () {
   var api;
 
   before(function () {
     api = new challonge.API(credentials.key);
   });
 
-  describe('Tests tournaments', function () {
-    it('Tests tournaments.index', function () {
-      api
-        .tournaments()
-        .index()
-        .then((tournaments) => assert.isArray(tournaments));
+  describe('tournaments()', function () {
+    describe('tournaments().index()', function () {
+      it('correctly returns tournaments', async function () {
+        let tournaments = await api
+          .tournaments()
+          .index()
+          .then((tournaments) => tournaments);
+        assert.isArray(tournaments);
+      });
+
+      let states = [
+        { state: 'all' },
+        { state: 'pending' },
+        { state: 'in_progress' },
+        { state: 'ended' },
+      ];
+      states.forEach(function (states) {
+        it(`correctly returns tournaments of state:${states.state}`, async function () {
+          let tournaments = await api
+            .tournaments()
+            .index(states.state)
+            .then((tournaments) => tournaments);
+          assert.isArray(tournaments);
+        });
+      });
+
+      let t_types = [
+        { t_type: 'single_elimination', expect: 'single elimination' },
+        { t_type: 'double_elimination', expect: 'double elimination' },
+        { t_type: 'round_robin', expect: 'round robin' },
+        { t_type: 'swiss', expect: 'swiss' },
+      ];
+      t_types.forEach(function (t_types) {
+        it(`correctly returns tournaments of type:${t_types.t_type}`, async function () {
+          let tournaments = await api
+            .tournaments()
+            .index(t_types.t_type)
+            .then((tournaments) => tournaments);
+          assert.isArray(tournaments);
+        });
+      });
+
+      it('correctly return tournaments of a subdomain', async function () {
+        let tournaments = await api
+          .tournaments()
+          .index((subdomain = 'reggiesroughriders'));
+        assert.isArray(tournaments);
+      });
     });
 
-    it('Tests tournaments.show', function () {
+    it('.show() returns a tournamennt', async () => {
       const url = 'reggiesroughriders';
-      api
-        .tournaments()
-        .show(url)
-        .then((tournament) => assert.equal(tournament.url, url));
+      const tournament = await api.tournaments().show(url);
+      assert.equal(tournament.url, url);
     });
   });
 
